988b6b05d6395bec226e45c7a1022286cc860297
masukoreo <0317masuo@gmail.com>
2022-06-21 21:24:21 +0900

    123

--- Homework1/src/main/java/Result1.java
+++ Homework1/src/main/java/Result1.java
@@ -1,5 +1,11 @@
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -30,6 +36,39 @@ public class Result1 extends HttpServlet {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
 		
+		String msg="";
+	    try {
+		      Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+		      msg = "ドライバのロードに成功しました";
+		    }catch (ClassNotFoundException e){
+		    	System.out.println(e.getMessage());
+		      msg = "ドライバのロードに失敗しました1";
+		    }catch (Exception e){
+		      msg = "ドライバのロードに失敗しました2";
+		    }
+		System.out.println(msg);
+		
+		try {
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/itemdb","root","");
+			
+			Statement stmt=con.createStatement(); 
+			
+			ResultSet rs=stmt.executeQuery("select * from item;");
+			
+			while (rs.next()) {
+	              System.out.println(rs.getInt("itemid"));
+	              System.out.println(rs.getString("item_name"));
+	            }
+			
+			rs.close();
+            stmt.close();
+            con.close();
+			
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		
 		String view = "/WEB-INF/view/result1.jsp";
 	    RequestDispatcher dispatcher = request.getRequestDispatcher(view);
 	    dispatcher.forward(request, response);
@@ -54,16 +93,48 @@ public class Result1 extends HttpServlet {
 		    }
 		System.out.println(msg);
 		
+		
+		
 		  request.setCharacterEncoding("utf-8");
 		  
-		  String num = request.getParameter("score");
+		  //String num = request.getParameter("score");
 		  
-		  int score = Integer.parseInt(num);
+		  //int score = Integer.parseInt(num);
 		  
-		  request.setAttribute("score",score );
+		  //request.setAttribute("score",score );
 		  
-		  doGet(request, response);
+		  String itemname =request.getParameter("item_name");
 		  
+		  String num2 =request.getParameter("itemid");
+		  
+		  String num =request.getParameter("price");
+		  
+		  int price = Integer.parseInt(num);
+		  
+		  int itemid = Integer.parseInt(num2);
+		  
+		  try {
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/itemdb","root","");
+		
+			String sql = "insert into item(itemid,item_name,price)values(?,?,?)";
+			
+			PreparedStatement st = con.prepareStatement(sql);
+			
+			st.setInt(1,itemid);
+			st.setString(2,itemname);
+			st.setInt(3,price);
+			
+			int result = st.executeUpdate();
+			System.out.println(result);
+			
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		  
+		 // doGet(request, response);
+		  
+	
 	}
 
 }
--- Homework1/src/main/webapp/WEB-INF/view/homework1.jsp
+++ Homework1/src/main/webapp/WEB-INF/view/homework1.jsp
@@ -6,9 +6,13 @@
 <meta charset="UTF-8">
 <title>Insert title here</title>
 </head>
-<body>
+<body>  
 	<form method="post" action="./result1">
-		点数(1~100)を入力してください!!<input type="text" name="score">
+		<!--点数(1~100)を入力してください!!<input type="text" name="score"> -->
+		
+		itemname <input type="text" name="item_name">
+		itemid <input type="text" name="itemid">
+		price <input type="text" name="price">
 		<button type="submit">送信</button>
 	</form>
 </body>
